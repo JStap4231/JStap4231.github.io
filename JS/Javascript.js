1f925569c11b181c3fe5f5070b917f6b75937591
JStap4231 <44093399+JStap4231@users.noreply.github.com>
2019-01-09 23:19:04 +0000

    .

--- JS/Javascript.js
+++ JS/Javascript.js
@@ -1,6 +1,9 @@
 window.onload = function() {
 
+    //Adds an event listener to the menu button on middle and waits for it to be touched
     document.getElementById("menu").addEventListener("touchstart", open);
+    
+    //Adds event listeners to the images on the customizer page
     document.getElementById("button1").addEventListener("click", draw2d);
     document.getElementById("button2").addEventListener("click", draw2d_1);
     document.getElementById("button3").addEventListener("click", draw2d_2);
@@ -14,18 +17,18 @@ window.onload = function() {
     document.getElementById("button11").addEventListener("click", drawmurdoc_1);
     document.getElementById("button12").addEventListener("click", drawmurdoc_2);
 
-    var canvas = document.getElementById("canvas");
-    context = canvas.getContext("2d");
-    width = canvas.width;
-    height = canvas.height;
+    var canvas = document.getElementById("canvas"); //sets canvas variable
+    context = canvas.getContext("2d"); //sets context variable
+    width = canvas.width; //sets width as the canvas width
+    height = canvas.height; //sets height as the canvas height
 
     
     function draw2d(){
-        context.clearRect(0, 0, width/2, height/2)
-        image = new Image();
-        image.src = 'Assets/2d.png';
-        image.onload = function(){
-            context.drawImage(image, 0, 0, width/2, height/2);
+        context.clearRect(0, 0, width/2, height/2) //clears this section of the canvas
+        image = new Image(); //initialises the image
+        image.src = 'Assets/2d.png'; //the source of the image
+        image.onload = function(){ //function will execute when the image has fully loaded
+            context.drawImage(image, 0, 0, width/2, height/2); //draws image to canvas at this location
         }
     }
     function draw2d_1(){
@@ -122,11 +125,11 @@ window.onload = function() {
     
     
     function open() {
-        var x = document.getElementById("drop");
-        if (x.style.display === "none") {
-            x.style.display = "block";
+        var x = document.getElementById("drop"); //gets the element
+        if (x.style.display === "none") { //checks to see if the drop-down mneu is visible
+            x.style.display = "block"; //will show the content of the dropdown menu
         } else {
-        x.style.display = "none";
+        x.style.display = "none"; //will hide the content of tehe drop-down menu
         }
     }  
 }
